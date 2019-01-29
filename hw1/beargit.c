ab7238f473e245cb5d1eb57c422c25d7dc3057e0
taciossbr <tacioss.br@gmail.com>
2019-01-29 15:21:44 -0200

    hw1

--- hw1/beargit.c
+++ hw1/beargit.c
@@ -95,7 +95,31 @@ int beargit_add(const char* filename) {
  */
 
 int beargit_rm(const char* filename) {
-  /* COMPLETE THE REST */
+  FILE* findex = fopen(".beargit/.index", "r");
+  FILE *fnewindex = fopen(".beargit/.newindex", "w");
+  char scan_set[15];
+  sprintf(scan_set, " %%%u[^\n]", FILENAME_SIZE);
+  char s[FILENAME_SIZE+1];
+  int sen = 0;
+  while(fscanf(findex, scan_set, s) > 0) {
+    if (strcmp(s, filename) == 0) {
+      sen = 1;
+    } else {
+      fprintf(fnewindex, "%s\n", s);
+    }
+  }
+  
+  fclose(findex);
+  fclose(fnewindex);
+
+  if(sen) {
+    fs_mv(".beargit/.newindex", ".beargit/.index");
+  } else {
+    fs_rm(".beargit/.newindex");
+    fprintf(stderr, "ERROR: File %s not tracked", filename);
+    
+    return 1;
+  }
 
   return 0;
 }
@@ -109,12 +133,70 @@ int beargit_rm(const char* filename) {
 const char* go_bears = "GO BEARS!";
 
 int is_commit_msg_ok(const char* msg) {
-  /* COMPLETE THE REST */
+  int i, j, sen = 0;
+  for(i = 0; msg[i] != '\0'; i++){
+    for(j = 0; msg[i+j] != '\0' && j < 9; j++){
+      if(go_bears[j] == msg[i+j]) {
+        sen += 1;
+      } else {
+        sen = 0;
+        break;
+      }
+    }
+    if(sen == 9) {
+      return 1;
+    } else {
+      sen = 0;
+    }
+  }
   return 0;
 }
 
 void next_commit_id(char* commit_id) {
-  /* COMPLETE THE REST */
+  char prev_commit_id[COMMIT_ID_SIZE];
+  read_string_from_file(".beargit/.prev", prev_commit_id, COMMIT_ID_SIZE);
+  
+  commit_id[COMMIT_ID_BYTES] = '\0';
+  int i = COMMIT_ID_BYTES - 1, carry = 0;
+  switch(prev_commit_id[i]) {
+    case '0':
+      commit_id[i] = '1';
+      break;
+    case '1':
+      commit_id[i] = '6';
+      break;
+    case '6':
+      commit_id[i] = 'c';
+      break;
+    case 'c':
+      commit_id[i] = '1';
+      carry = 1;
+  }
+  for(i -= 1; i > -1 ; i--) {
+    if (carry) {
+      switch(prev_commit_id[i]) {
+        case '0':
+          commit_id[i] = '1';
+          carry = 0;
+          break;
+        case '1':
+          commit_id[i] = '6';
+          carry = 0;
+          break;
+        case '6':
+          commit_id[i] = 'c';
+          carry = 0;
+          break;
+        case 'c':
+          commit_id[i] = '1';
+          carry = 1;
+      }
+    } else if(prev_commit_id[i] == '0') {
+      commit_id[i] = '1';
+    } else {
+      commit_id[i] = prev_commit_id[i];
+    }
+  }
 }
 
 int beargit_commit(const char* msg) {
@@ -126,8 +208,43 @@ int beargit_commit(const char* msg) {
   char commit_id[COMMIT_ID_SIZE];
   read_string_from_file(".beargit/.prev", commit_id, COMMIT_ID_SIZE);
   next_commit_id(commit_id);
+  
+  char prev_commit_id[COMMIT_ID_SIZE];
+  read_string_from_file(".beargit/.prev", prev_commit_id, COMMIT_ID_SIZE);
 
-  /* COMPLETE THE REST */
+  char commit_dir[10+COMMIT_ID_SIZE];
+  sprintf(commit_dir, ".beargit/%s/", commit_id);
+  
+  fs_mkdir(commit_dir);
+  
+  char dest[10+COMMIT_ID_SIZE+FILENAME_SIZE];
+  
+  strcpy(dest, commit_dir);
+  strcat(dest, ".index");
+  fs_cp(".beargit/.index", dest);
+  
+  strcpy(dest, commit_dir);
+  strcat(dest, ".prev");
+  fs_cp(".beargit/.prev", dest);
+  
+  FILE* findex = fopen(".beargit/.index", "r");
+  char scan_set[15];
+  sprintf(scan_set, " %%%u[^\n]", FILENAME_SIZE);
+  char src[FILENAME_SIZE];
+  while(fscanf(findex, scan_set, src) > 0) {
+    strcpy(dest, commit_dir);
+    strcat(dest, src);
+    
+    fs_cp(src, dest);
+  }
+  
+  fclose(findex);
+  write_string_to_file(".beargit/.prev", commit_id);
+  
+  strcpy(dest, commit_dir);
+  strcat(dest, ".msg");
+  write_string_to_file(dest, msg);
+
 
   return 0;
 }
@@ -139,7 +256,25 @@ int beargit_commit(const char* msg) {
  */
 
 int beargit_status() {
-  /* COMPLETE THE REST */
+  char scan_set[15];
+  sprintf(scan_set, " %%%u[^\n]", FILENAME_SIZE);
+  
+  FILE *f;
+  if((f = fopen(".beargit/.index", "r")) == NULL) {
+    fprintf(stderr, "\nErro: .beargit/.index nao encontrado!\n");
+    fprintf(stderr, "\n      Voce ja inicializou o\n");
+    return 2;
+  }
+  
+  printf("Tracked files:\n\n");
+  
+  int c = 0;
+  char s[FILENAME_SIZE+1];
+  while(fscanf(f, scan_set, s) > 0) {
+    printf("\t%s\n", s);
+    c++;
+  }
+  printf("\n%d files total\n", c);
 
   return 0;
 }
@@ -151,7 +286,34 @@ int beargit_status() {
  */
 
 int beargit_log() {
-  /* COMPLETE THE REST */
+
+  char commit_id[COMMIT_ID_SIZE];
+  read_string_from_file(".beargit/.prev", commit_id, COMMIT_ID_SIZE);
+  if(!strcmp("0000000000000000000000000000000000000000", commit_id)) {
+    fprintf(stderr, "ERROR: There are no commits!");
+    return 1;
+  }
+  while(strcmp("0000000000000000000000000000000000000000", commit_id)) {
+    putchar('\n');
+    printf("commit %s\n", commit_id);
+    char msg[MSG_SIZE];
+    char commit_dir[14+COMMIT_ID_SIZE];
+    sprintf(commit_dir, ".beargit/%s/", commit_id);
+    
+    char src[15+COMMIT_ID_SIZE];
+    strcpy(src, commit_dir);
+    strcat(src, ".msg");
+    read_string_from_file(src, msg, MSG_SIZE);
+    
+    printf("\t%s\n", msg);
+    
+    strcpy(src, commit_dir);
+    strcat(src, ".prev");
+    
+    read_string_from_file(src, commit_id, COMMIT_ID_SIZE);
+  }
+  
+  
 
   return 0;
 }
